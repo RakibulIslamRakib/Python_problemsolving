859206002cb0e889620b23060fe0bb53c63311c0
RakibulIslamRakib <56424436+RakibulIslamRakib@users.noreply.github.com>
2019-10-24 07:30:43 -0700

    Update 706BInteresting drink.py

--- 706BInteresting drink.py
+++ 706BInteresting drink.py
@@ -1,6 +1,7 @@
 from bisect import bisect_right
 #bisect_right func have 2parameter. first one is the array and second one is a value. 
 #This function return the location where the value should be insert while it is in shorted
+#print(bisect_left(l, x))//it will return the  position jar por e value ta insert hobe
 
 n = int(input())
 x = list(sorted(map(int, input().split())))
